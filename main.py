f93b30e5243206e10f5d57817de38996235e2c3d
5m0ky <smoxics@gmail.com>
2020-11-26 20:56:19 +0530

    Second commit

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-import threading
+from threading import *
 import optparse
 import socket
 import sys
 import os 
 
+screenLock = Semaphore(value=1)
 def connScan(target_host, target_port):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         s.connect((target_host, target_port))
+        s.send(b"donttmindme\r\n")
         print(f"{target_port} open")
-        s.send(b"EHLO\r\n")
         response = s.recv(1024)
-        if res:
-            print(str(response))
-        else:
-            pass
-        s.close()
+        screenLock.acquire()
+        print(response.decode())
     except:
+        screenLock.acquire()
         print(f"{target_port} not open on {target_host}")
+    finally:
+        screenLock.release()
+        s.close()
 
 def portScan(hostname, ports):
     target_ip = socket.gethostbyname(hostname)
@@ -30,21 +32,24 @@ def portScan(hostname, ports):
         print(f"Scan results for {target_name[0]}")
     except:
         print(f"Scan results for {target_ip}")
+    socket.setdefaulttimeout(1)
     for port in ports:
         print(f"Scanning port {port}")
-        connScan(hostname, int(port))
+        t = Thread(target=connScan, args=(hostname, int(port)))
+        t.start()
+        #connScan(hostname, int(port))
 
-if __name__ == "__main__":
-    parser = optparse.OptionParser("[*] Usage: %prog -H '<target host>' '<target port>'")
+def main():
+    parser = optparse.OptionParser("[*] Usage: %prog -H '<target host>' -p '<target port>'")
     parser.add_option("-H", type="string", help="Specify Target Hostname", dest="target_host")
-    parser.add_option("-P", type="string", help="Specify Target Port", dest="target_port")
+    parser.add_option("-p", type="string", help="Specify Target Port", dest="target_port")
     (options, args) = parser.parse_args()
-    #tHost = options.target_host
-    #tPort = str(options.target_port)
-    tHost = "192.168.56.1"
-    tPort = "135, 445, 5050, 8080, 80".split(", ")
+    tHost = options.target_host
+    tPort = str(options.target_port).split(', ')
     if (tHost == None) | (tPort == None):
         print(parser.usage)
         exit(0)
-    else:
-        portScan(tHost, tPort)
+    portScan(tHost, tPort)
+
+if __name__ == "__main__":
+    main()
